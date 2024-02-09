1cb4dd54a0b867ddbdbdb1f85043dd5dbf0c3bcc
tajtanvvir1 <tanvirah1223@gmail.com>
2024-02-09 16:39:28 +0600

    Prb 5 running

--- prb5.js
+++ prb5.js
@@ -13,8 +13,19 @@ function monthlySavings(incomes, livingCost){
         return "invalid input"
     }
 
-    const 
-    return "ok"
-}
+    for (let i=0 ; i < incomes.length; i++){
+       if (incomes[i] >= 3000){
+            incomes[i] -= incomes[i] * .2
+        }
+    }
 
-console.log(monthlySavings([5], []))
+    for (let i=0 ; i < incomes.length; i++){
+
+    }
+    
+    
+    // const savings = incomes - livingCost;
+    // return incomes
+}
+// console.log(savings)
+console.log(monthlySavings([2000, 1000], 2))
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-function cutTwentyPercentInPlace(arr) {
+/* function cutTwentyPercentInPlace(arr) {
   const reductionPercentage = 0.2;
   const threshold = 3000;
 
@@ -16,3 +16,17 @@ const numbersArray = [2500, 3500, 2800, 4000];
 cutTwentyPercentInPlace(numbersArray);
 console.log(numbersArray);
 
+*/
+
+function sumArrayElements(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+  }
+  return sum;
+}
+
+// Example usage:
+const numbersArray = [1, 2, 3, 4, 5];
+const resultSum = sumArrayElements(numbersArray);
+console.log(resultSum);
